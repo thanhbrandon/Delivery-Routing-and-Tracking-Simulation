e9dfe61e9ce8eab6ea114f65dda37248cdccf30e
thanhbrandon <thanh.brandon.tran@gmail.com>
2024-09-12 01:24:31 -0500

    Update README to include Scenario and Assumptions

--- Delievery Routing and Tracking Simulation/Delivery_Routing_and_Tracking_Simulation.py
+++ Delievery Routing and Tracking Simulation/Delivery_Routing_and_Tracking_Simulation.py
@@ -1,1 +1,138 @@
-print("Hello World")
+# C950 - Webinar-1 - Let’s Go Hashing
+# W-1_ChainingHashTable_zyBooks_Key-Value.py
+# Ref: zyBooks: Figure 7.8.2: Hash table using chaining.
+# Modified for Key:Value
+
+# HashTable class using chaining.
+class ChainingHashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    def __init__(self, initial_capacity=10):
+        # initialize the hash table with empty bucket list entries.
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # Inserts a new item into the hash table.
+    ''' 
+    #Original
+    def insert(self, item):
+        # get the bucket list where this item will go.
+        bucket = hash(item) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # insert the item to the end of the bucket list.
+        bucket_list.append(item)
+    '''
+
+    def insert(self, key, item):  # does both insert and update
+        # get the bucket list where this item will go.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # update key if it is already in the bucket
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        # if not, insert the item to the end of the bucket list.
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
+    '''
+        # Original
+        def search(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        print(bucket_list)
+
+        # search for the key in the bucket list
+        if key in bucket_list:
+            # find the item's index and return the item that is in the bucket list.
+            item_index = bucket_list.index(key)
+            return bucket_list[item_index]
+        else:
+            # the key is not found.
+            return None
+    '''
+
+    def search(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        # print(bucket_list)
+
+        # search for the key in the bucket list
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                return kv[1]  # value
+        return None
+
+    # Removes an item with matching key from the hash table.
+    '''
+        def remove(self, key):
+        # get the bucket list where this item will be removed from.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # remove the item from the bucket list if it is present.
+        if key in bucket_list:
+            bucket_list.remove(key)
+    '''
+
+    def remove(self, key):
+        # get the bucket list where this item will be removed from.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # remove the item from the bucket list if it is present.
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                bucket_list.remove([kv[0], kv[1]])
+
+
+bestMovies = [
+    [1, "CITIZEN KANE - 1941"],
+    [2, "CASABLANCA - 1942"],
+    [3, "THE GODFATHER - 1972"],
+    [4, "GONE WITH THE WIND - 1939"],
+    [5, "LAWRENCE OF ARABIA - 1962"],
+    [6, "THE WIZARD OF OZ - 1939"],
+    [7, "THE GRADUATE - 1967"],
+    [8, "ON THE WATERFRONT- 1954"],
+    [9, "SCHINDLER'S LIST -1993"],
+    [10, "SINGIN' IN THE RAIN - 1952"],
+    [11, "STAR WARS - 1977"]
+]
+
+myHash = ChainingHashTable()
+
+print("\nInsert:")
+myHash.insert(bestMovies[0][0], bestMovies[0][1])  # 2nd bucket; Key=1, item="CITIZEN KANE - 1941"
+print(myHash.table)
+
+myHash.insert(bestMovies[10][0], bestMovies[10][1])  # 2nd bucket as well; Key=11, item="STAR WARS - 1977"
+print(myHash.table)
+
+print("\nSearch:")
+print(myHash.search(1))  # Key=1, item="CITIZEN KANE - 1941"
+print(myHash.search(11))  # Key=11, item="STAR WARS - 1977"; so same bucket and Chainin is working
+
+print("\nUpdate:")
+myHash.insert(1, "Star Trek - 1979")  # 2nd bucket; Key=1, item="Star Trek - 1979"
+print(myHash.table)
+
+print("\nRemove:")
+myHash.remove(1)  # Key=1, item="Star Trek - 1979" to remove
+print(myHash.table)
+
+myHash.remove(11)  # Key=11, item="STAR WARS - 1977" to remove
+print(myHash.table)
