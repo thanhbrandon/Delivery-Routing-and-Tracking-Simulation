ea61a687fde8412cd663914842d079c488903918
thanhbrandon <thanh.brandon.tran@gmail.com>
2024-09-19 23:59:14 -0500

    Updated truck class

--- Delievery Routing and Tracking Simulation/truck.py
+++ Delievery Routing and Tracking Simulation/truck.py
@@ -2,6 +2,7 @@ class Truck:
 
     def __init__(self):
         self.numPackages = 0
-
+        self.maxPackages = 16
+        self.mph = 18
     def getNumPackages(self):
         print("There are no packages.")
