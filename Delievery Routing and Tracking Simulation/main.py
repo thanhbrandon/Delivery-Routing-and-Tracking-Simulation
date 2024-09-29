30db2c4b5888c91d4528fd373892f68a27363e2c
thanhbrandon <thanhbrandon@tamu.edu>
2024-09-29 02:16:32 -0500

    Update Project Removed files Updated main functions

--- Delievery Routing and Tracking Simulation/main.py
+++ Delievery Routing and Tracking Simulation/main.py
@@ -1,8 +1,69 @@
 from truck import Truck
-from hashtable import HashTable
-from file import File
 from package import Package
-from distance import Distance
+from hashtable import ChainingHashTable
+from datetime import datetime, time, timedelta
+
+
+import csv
+
+# csv file names
+packageFile = "WGUPS Package File.csv"
+distanceFile = "WGUPS Distance Table.csv"
+addressFile = "addresses.csv"
+
+# initializing hast table
+packageHash = ChainingHashTable()
+
+# reading csv file
+with open(packageFile, 'r', encoding='utf-8-sig') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting each data row one by one
+    for row in csvreader:
+        key = int(row[0])
+        package_id = row[0]
+        address = row[1]
+        city = row[2]
+        state = row[3]
+        zip_code = row[4]
+        delivery_time = row[5]
+        weight = row[6]
+        status = 'At HUB'
+        notes = row[7]
+        newPackage = Package(package_id, address, city, state, zip_code, delivery_time, weight, status, notes)
+        packageHash.insert(key, newPackage)
+
+
+
+with open(addressFile,'r',encoding='utf-8-sig') as csvfile:
+    addressRef = csv.reader(csvfile)
+    key = []
+    address = []
+    for row in addressRef:
+        if row:
+            key.append(row[0])
+            address.append(row[1].strip())
+
+
+print(packageHash.search(6))
+
+# Loading packages into trucks manually
+# Packages 6,25,28,32 are delayed and not arriving until 9:05am 
+truck1 = Truck("Truck 1", # Package 14 must be delivered with 15,19 and Package 20 must be delivered with 13,15
+                  [1,2,13,14,15,19,20,21,27,29,30,33,34,35,37],
+                  datetime(2024, 8, 23,8,0))
+
+truck2 = Truck("Truck 2", # Packages 36,38,18,3 can only be on truck 2
+                  [4,6,11,12,17,18,17,18,23,24,31,32,36,40,3,38],
+                  datetime(2024,8,23,9,5))
+
+truck3 = Truck("Truck 3",# Package 16 must be delivered with 13,19
+                    [5,7,8,9,10,16,26,28,39,25,22],
+                    datetime(2024,8,23,9,50)) # placeholder - truck 3 needs to wait until truck 1 returns
+
+print(truck1)
+
 #totalDistance = 0
 #		currentLocation = Packages[hub]
 #		while totalPackages > 0
@@ -14,65 +75,26 @@ from distance import Distance
 #					package.drop[nextLocation]
 #				totalDistance = totalDistance + nextlocation.distance[0]
 
-truck_1 = Truck()
+with open(distanceFile, 'r', encoding='utf-8-sig') as csvfile:
+    distanceRef = csv.reader(csvfile)
+    distanceRef = list(distanceRef)
 
-print(truck_1.numPackages)
+def findDistance(row, column):
+    distance = distanceRef[row][column]
+    return float(distance)
 
-truck_1.getNumPackages()
+print(findDistance(2,1))
 
-bestMovies = [
-    [1, "CITIZEN KANE - 1941"],
-    [2, "CASABLANCA - 1942"],
-    [3, "THE GODFATHER - 1972"],
-    [4, "GONE WITH THE WIND - 1939"],
-    [5, "LAWRENCE OF ARABIA - 1962"],
-    [6, "THE WIZARD OF OZ - 1939"],
-    [7, "THE GRADUATE - 1967"],
-    [8, "ON THE WATERFRONT- 1954"],
-    [9, "SCHINDLER'S LIST -1993"],
-    [10, "SINGIN' IN THE RAIN - 1952"],
-    [11, "STAR WARS - 1977"]
-]
-
-myHash = HashTable()
-
-print("\nInsert:")
-myHash.insert(bestMovies[0][0], bestMovies[0][1])  # 2nd bucket; Key=1, item="CITIZEN KANE - 1941"
-print(myHash.table)
-
-myHash.insert(bestMovies[10][0], bestMovies[10][1])  # 2nd bucket as well; Key=11, item="STAR WARS - 1977"
-print(myHash.table)
-
-print("\nSearch:")
-print(myHash.search(1))  # Key=1, item="CITIZEN KANE - 1941"
-print(myHash.search(11))  # Key=11, item="STAR WARS - 1977"; so same bucket and Chainin is working
-
-print("\nUpdate:")
-myHash.insert(1, "Star Trek - 1979")  # 2nd bucket; Key=1, item="Star Trek - 1979"
-print(myHash.table)
-
-print("\nRemove:")
-myHash.remove(1)  # Key=1, item="Star Trek - 1979" to remove
-print(myHash.table)
-
-myHash.remove(11)  # Key=11, item="STAR WARS - 1977" to remove
-print(myHash.table)
-
-# RUNTIME -> O(n^2)
-def load_package_data():
-    package_data = File('WGUPS Package File.csv')
-    size = package_data.get_row_count()
-    package_hash_table = HashTable(size)
-    package_hash_table = package_data.parse_package_data()
-    return package_hash_table
-
-# RUNTIME -> O(n^2)
-def load_distance_table():
-    distance_table = File('WGUPS Distance Table.csv')
-    raw_data = distance_table.parse_distance_data()
-    d = Distance(raw_data)
-    distance_data = d.clean_and_sort_data()
-    return distance_data
-
-load_package_data()
-load_distance_table()
+# choosing the package closest to the location that the truck is currently at (greedy method)
+def nearestLocation(start, package_list):
+    smallest_distance = float('inf')
+    next_location = None
+    start_key = address_to_key(start)
+    for package in package_list:
+        id_to_key = address_to_key(package)
+        if id_to_key is not None:
+            distance = location_distances(start_key, id_to_key)
+            if distance < smallest_distance:
+                smallest_distance = distance
+                next_location = package
+    return next_location
--- Delievery Routing and Tracking Simulation/truck.py
+++ Delievery Routing and Tracking Simulation/truck.py
@@ -1,8 +1,15 @@
 class Truck:
 
-    def __init__(self):
-        self.numPackages = 0
-        self.maxPackages = 16
-        self.mph = 18
-    def getNumPackages(self):
-        print("There are no packages.")
+    def __init__(self, id, packages , departTime):
+        self.id = id
+        self.packages = packages
+        self.departTime = departTime
+
+        self.location = "4001 South 700 East",
+        self.mileage = 0.0
+        self.speed = 18
+
+    def __str__(self):
+        return (f"Truck ID: {self.id}, Truck Speed: {self.speed}, Mileage: {self.mileage}, "
+                f"Current Location: {self.location}, Packages: {self.packages} , "
+                f"Time of Departure: {self.departTime}")   
